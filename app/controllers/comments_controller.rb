63586cb294dae85d5e529b6f4010a6bb7270026d
Luke <lukechinworth@gmail.com>
2015-03-20 17:27:54 -0700

    add commenting, editing, and deletion for comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,49 @@
+class CommentsController < ApplicationController
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @post.comments.new(comment_params)
+    if @post.save
+      flash[:notice] = "Comment posted."
+      redirect_to post_path(@post)
+    else
+      flash[:alert] = "Comment failed to post."
+      redirect_to post_path(@post)
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    if @comment.update(comment_params)
+      flash[:notice] = "Comment updated."
+      redirect_to post_path(@post)
+    else
+      flash[:alert] = "Comment failed to update."
+      redirect_to post_path(@post)
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    flash[:notice] = "Comment successfully deleted."
+    redirect_to post_path(@post)
+  end
+
+private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+end
