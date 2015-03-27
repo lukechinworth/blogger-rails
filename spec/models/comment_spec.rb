3f6dc00e074f4a2141fd9d6726033bac645f4463
Luke Chinworth <lukechinworth@gmail.com>
2015-03-27 10:23:23 -0700

    add devise user and associations to posts and comments

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,5 +2,6 @@ require 'rails_helper'
 
 describe Comment do
   it { should belong_to :post }
+  it { should belong_to :user }
   it { should validate_presence_of :body }
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 describe Post do
-  it { should have_many :comments}
+  it { should have_many :comments }
+  it { should belong_to :user }
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
 end
